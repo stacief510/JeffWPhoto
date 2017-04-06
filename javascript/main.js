f656b66b8cd50b03f3f2524f62ccb62e08891c1f
stacief510 <staciemfraser@gmail.com>
2017-04-05 17:39:32 -0700

    favicon
    adding a favicon

--- javascript/main.js
+++ javascript/main.js
@@ -9,7 +9,7 @@ $('#showModal').on('click', function() {
 $('#close').on('click', function () {
 $('.modal-background').hide();
 $('.modal-content').hide();
-})
+});
 
 //for dropdown menu:
 
@@ -18,5 +18,33 @@ $('.modal-content').hide();
 // 	$('.dropdown').toggle();
 // 	})
 // })
+
+
+//for client page
+
+$('.unlock').on('click', function (event) {
+	event.preventDefault();
+
+var required = $('.required').val();
+var required2 = $('.required2').val();
+console.log(required);
+console.log(required2);
+
+
+if (required.length === 0) {
+	$('.required').addClass('error');
+}
+else {
+	$('.required').removeClass('error');
+}
+if (required2.length === 0){
+	$('.required2').addClass('error');
+}
+else {
+	$('.required2').removeClass('error');
+}
+});
+
+
 
 
